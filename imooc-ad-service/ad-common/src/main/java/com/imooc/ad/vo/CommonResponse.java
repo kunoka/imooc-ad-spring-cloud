e75f24ca13a9f84b2bb1d8f6e6833d8b84735696
Harry <hengwang@cn.ibm.com>
2019-11-18 17:26:26 +0800

    5-6 数据表实体类定义（2）

---
+++ imooc-ad-service/ad-common/src/main/java/com/imooc/ad/vo/CommonResponse.java
@@ -0,0 +1,21 @@
+package com.imooc.ad.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data //对各个属性实现set, get
+@NoArgsConstructor //无参的构造函数
+@AllArgsConstructor //全参的构造函数
+public class CommonResponse<T> implements Serializable {
+  private Integer code;
+  private String message;
+  private T data;
+
+  public CommonResponse(Integer code, String message) {
+    this.code = code;
+    this.message = message;
+  }
+}
---
+++ imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/SponsorApplication.java
@@ -0,0 +1,18 @@
+package com.imooc.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients //这个注解，这个微服务可以调用其他微服务，在dashboard中可以实现监控
+@EnableCircuitBreaker //调度器，也是为了实现监控
+@EnableEurekaClient //标识它是一个eureka client
+@SpringBootApplication //这是一个springboot的application
+public class SponsorApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringApplication.class, args);
+  }
+
+}
