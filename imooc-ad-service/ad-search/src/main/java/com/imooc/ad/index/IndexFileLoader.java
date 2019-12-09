6fd517495a0bae6596fc7ac09718a4937e9af088
Harry <hengwang@cn.ibm.com>
2019-12-09 10:26:56 +0800

    8-10 全量索引加载的实现（1）

---
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/index/IndexFileLoader.java
@@ -0,0 +1,74 @@
+package com.imooc.ad.index;
+
+import com.alibaba.fastjson.JSON;
+import com.imooc.ad.dump.DConstant;
+import com.imooc.ad.dump.table.AdCreativeTable;
+import com.imooc.ad.dump.table.AdCreativeUnitTable;
+import com.imooc.ad.dump.table.AdPlanTable;
+import com.imooc.ad.dump.table.AdUnitTable;
+import com.imooc.ad.handler.AdLevelDataHandler;
+import com.imooc.ad.mysql.constant.OpType;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 根据数据表导出的文件，读取文件，加载索引
+ */
+@Component
+@DependsOn("dataTable") //定义依赖关系
+public class IndexFileLoader {
+  public void init(){
+    List<String> adPlanString = loadDumpData(
+      String.format("%s%s",
+        DConstant.DATA_ROOT_DIR,
+        DConstant.AD_PLAN)
+    );
+    adPlanString.forEach(p -> AdLevelDataHandler.handleLevel2(
+      JSON.parseObject(p, AdPlanTable.class),
+      OpType.ADD
+    ));
+    List<String> adCreativeStrings = loadDumpData(
+      String.format("%s%s",
+        DConstant.DATA_ROOT_DIR,
+        DConstant.AD_CREATIVE)
+    );
+    adCreativeStrings.forEach(c -> AdLevelDataHandler.handleLevel2(
+      JSON.parseObject(c, AdCreativeTable.class),
+      OpType.ADD
+    ));
+    List<String> adUnitStrings = loadDumpData(
+      String.format("%s%s",
+        DConstant.DATA_ROOT_DIR,
+        DConstant.AD_UNIT)
+    );
+    adUnitStrings.forEach(u -> AdLevelDataHandler.handleLevel3(
+      JSON.parseObject(u, AdUnitTable.class),
+      OpType.ADD
+    ));
+    List<String> adCreativeUnitStrings = loadDumpData(
+      String.format("%s%s",
+        DConstant.DATA_ROOT_DIR,
+        DConstant.AD_CREATIVE_UNIT)
+    );
+    adCreativeUnitStrings.forEach(c -> AdLevelDataHandler.handleLevel3(
+      JSON.parseObject(c, AdCreativeUnitTable.class),
+      OpType.ADD
+    ));
+  }
+  private List<String> loadDumpData(String fileName){
+    try(BufferedReader br = Files.newBufferedReader(
+      Paths.get(fileName)
+    )){
+      return br.lines().collect(Collectors.toList());
+    }catch (IOException ex){
+      throw new RuntimeException(ex.getMessage());
+    }
+  }
+}
