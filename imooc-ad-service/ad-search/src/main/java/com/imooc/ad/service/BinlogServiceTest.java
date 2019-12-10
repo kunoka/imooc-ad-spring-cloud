197ffc40d966c70e218b8ba17b97af0af1e1360b
Harry <hengwang@cn.ibm.com>
2019-12-11 07:13:16 +0800

    9-6 关于 Binlog 监听与解析的说明

--- imooc-ad-service/ad-search/src/main/java/com/imooc/ad/service/BinlogServiceTest.java
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/service/BinlogServiceTest.java
@@ -12,19 +12,20 @@ public class BinlogServiceTest {
   public static void main(String[] args) throws Exception {
     System.out.println("===开始执行===");
     BinaryLogClient client = new BinaryLogClient(
-       "127.0.0.1",3306, "root", "password"
+      "127.0.0.1", 3306, "root", "password"
     );
-//    client.setBinlogFilename();
+    client.setBinlogFilename("binlog.000037");
 //    client.setBinlogPosition();
+    //注册事件监听器
     client.registerEventListener(event -> {
       EventData data = event.getData();
-      if(data instanceof UpdateRowsEventData){
+      if (data instanceof UpdateRowsEventData) {
         System.out.println("Update-----------------------");
         System.out.println(data.toString());
-      }else if(data instanceof WriteRowsEventData){
+      } else if (data instanceof WriteRowsEventData) {
         System.out.println("Write-----------------------");
         System.out.println(data.toString());
-      }else if(data instanceof DeleteRowsEventData){
+      } else if (data instanceof DeleteRowsEventData) {
         System.out.println("Delete-----------------------");
         System.out.println(data.toString());
       }
