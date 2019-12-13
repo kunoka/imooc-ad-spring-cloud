a84d08fae5d6f855da857579ad7d8cee136057aa
Harry <hengwang@cn.ibm.com>
2019-12-13 10:22:23 +0800

    11-5 根据匹配信息实现对推广单元的再筛选

--- imooc-ad-service/ad-search/src/main/java/com/imooc/ad/search/impl/SearchImpl.java
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/search/impl/SearchImpl.java
@@ -2,6 +2,9 @@ package com.imooc.ad.search.impl;
 
 import com.imooc.ad.index.DataTable;
 import com.imooc.ad.index.adunit.AdUnitIndex;
+import com.imooc.ad.index.district.UnitDistrictIndex;
+import com.imooc.ad.index.interest.UnitItIndex;
+import com.imooc.ad.index.keyword.UnitKeywordIndex;
 import com.imooc.ad.search.ISearch;
 import com.imooc.ad.search.vo.SearchRequest;
 import com.imooc.ad.search.vo.SearchResponse;
@@ -11,12 +14,10 @@ import com.imooc.ad.search.vo.feature.ItFeature;
 import com.imooc.ad.search.vo.feature.KeywordFeature;
 import com.imooc.ad.search.vo.media.AdSlot;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Slf4j
 @Service
@@ -41,7 +42,93 @@ public class SearchImpl implements ISearch {
       Set<Long> adUnitIdSet = DataTable.of(
         AdUnitIndex.class
       ).match(adSlot.getPositionType());
+      if (relation == FeatureRelation.AND) {
+        filterKeywordFeature(adUnitIdSet, keywordFeature);
+        filterDistrictFeature(adUnitIdSet, districtFeature);
+        filterItTagFeature(adUnitIdSet, itFeature);
+
+        targetUnitIdSet = adUnitIdSet;
+      } else {
+        targetUnitIdSet = getORRelationUnitId(
+          adUnitIdSet,
+          keywordFeature,
+          districtFeature,
+          itFeature
+        );
+      }
     }
     return null;
+  }
+
+  private Set<Long> getORRelationUnitId(Set<Long> adUnitIdSet,
+                                        KeywordFeature keywordFeature,
+                                        DistrictFeature districtFeature,
+                                        ItFeature itFeature) {
+    if (CollectionUtils.isEmpty(adUnitIdSet)) {
+      return Collections.emptySet();
+    }
+    Set<Long> keywordUnitIdSet = new HashSet<>(adUnitIdSet);
+    Set<Long> districtUnitIdSet = new HashSet<>(adUnitIdSet);
+    Set<Long> itUnitIdSet = new HashSet<>(adUnitIdSet);
+
+    filterKeywordFeature(keywordUnitIdSet, keywordFeature);
+    filterDistrictFeature(districtUnitIdSet, districtFeature);
+    filterItTagFeature(itUnitIdSet, itFeature);
+
+    return new HashSet<>(
+      CollectionUtils
+        .union(
+          CollectionUtils.union(keywordUnitIdSet, districtUnitIdSet),
+          itUnitIdSet
+        )
+    );
+  }
+
+  private void filterKeywordFeature(
+    Collection<Long> adUnitIds, KeywordFeature keywordFeature) {
+    if (CollectionUtils.isEmpty(adUnitIds)) {
+      return;
+    }
+    if (CollectionUtils.isNotEmpty(keywordFeature.getKeywords())) {
+      CollectionUtils.filter(
+        adUnitIds,
+        adUnitId ->
+          DataTable.of(UnitKeywordIndex.class)
+            .match(adUnitId,
+              keywordFeature.getKeywords())
+      );
+    }
+  }
+
+  private void filterDistrictFeature(
+    Collection<Long> adUnitIds, DistrictFeature districtFeature) {
+    if (CollectionUtils.isEmpty(adUnitIds)) {
+      return;
+    }
+    if (CollectionUtils.isNotEmpty(districtFeature.getDistricts())) {
+      CollectionUtils.filter(
+        adUnitIds,
+        adUnitId ->
+          DataTable.of(UnitDistrictIndex.class)
+            .match(adUnitId,
+              districtFeature.getDistricts())
+      );
+    }
+  }
+
+  private void filterItTagFeature(
+    Collection<Long> adUnitIds, ItFeature itFeature) {
+    if (CollectionUtils.isEmpty(adUnitIds)) {
+      return;
+    }
+    if (CollectionUtils.isNotEmpty(itFeature.getIts())) {
+      CollectionUtils.filter(
+        adUnitIds,
+        adUnitId ->
+          DataTable.of(UnitItIndex.class)
+            .match(adUnitId,
+              itFeature.getIts())
+      );
+    }
   }
 }
