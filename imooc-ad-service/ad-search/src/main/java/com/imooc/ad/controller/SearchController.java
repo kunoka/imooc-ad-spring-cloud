e510e716e1b2fcd71e68a67862e4ef01cd3a1491
Harry <hengwang@cn.ibm.com>
2019-12-16 10:42:13 +0800

    11-8 完善广告检索服务

--- imooc-ad-service/ad-search/src/main/java/com/imooc/ad/controller/SearchController.java
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/controller/SearchController.java
@@ -5,7 +5,12 @@ import com.imooc.ad.IgnoreResponseAdvice;
 import com.imooc.ad.client.SponsorClient;
 import com.imooc.ad.client.vo.AdPlan;
 import com.imooc.ad.client.vo.AdPlanGetRequest;
+import com.imooc.ad.search.ISearch;
+import com.imooc.ad.search.vo.SearchRequest;
+import com.imooc.ad.search.vo.SearchResponse;
 import com.imooc.ad.vo.CommonResponse;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,13 +23,25 @@ import java.util.List;
 @Slf4j
 @RestController
 public class SearchController {
+
   private final RestTemplate restTemplate;
   private final SponsorClient sponsorClient;
+  private final ISearch search;
 
-  public SearchController(RestTemplate restTemplate, SponsorClient sponsorClient) {
+  @Autowired
+  public SearchController(RestTemplate restTemplate, SponsorClient sponsorClient, ISearch search) {
     this.restTemplate = restTemplate;
     this.sponsorClient = sponsorClient;
+    this.search = search;
   }
+
+  // post 方法 定义 url 为 fetchAds
+//  @PostMapping("/fetchAds")
+//  public SearchResponse fetchAds(@RequestBody SearchRequest request) {
+//    log.info("ad search: fetchAds -> {}",
+//      JSON.toJSONString(request));
+//    return search.fetchAds(request);
+//  }
 
   @IgnoreResponseAdvice
   @PostMapping("/getAdPlans")
